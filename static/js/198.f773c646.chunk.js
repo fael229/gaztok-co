7b7092b2cf80d5dc3d412d6003f139a093786ee1
fael229 <faeltoutcourt@gmail.com>
2024-11-30 02:04:54 +0100

    Updates

---
+++ static/js/198.f773c646.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgaztok_web=self.webpackChunkgaztok_web||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.f773c646.chunk.js.map
